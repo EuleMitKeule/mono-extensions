794b075dd66e78d354aa2fde86769cc690635705
Lennard Beers <l.beers@outlook.de>
2021-07-27 01:47:39 +0200

    implemented Component.GetCachedComponent

--- Runtime/ComponentExtensions.cs
+++ Runtime/ComponentExtensions.cs
@@ -7,6 +7,27 @@ namespace UnityExtensions.Runtime
 {
     public static class ComponentExtensions
     {
+        static Dictionary<GameObject, List<Component>> CachedComponents { get; } = new Dictionary<GameObject, List<Component>>();
+
+        public static T GetCachedComponent<T>(this Component component) where T : Component
+        {
+            var gameObject = component.gameObject;
+
+            if (!CachedComponents.ContainsKey(gameObject))
+            {
+                CachedComponents.Add(gameObject, new List<Component>());
+            }
+
+            var components = CachedComponents[gameObject];
+            var cachedComponent = components.Find(e => e is T);
+
+            cachedComponent ??= gameObject.GetComponent<T>();
+
+            if (cachedComponent) components.Add(cachedComponent);
+
+            return cachedComponent as T;
+        }
+
         public static void AddComponent<T>(this Component component) where T : Component =>
             component.gameObject.AddComponent<T>();
 
